5da3368b536dcee0ff7c3680586a3da642b8ce22
NI Tushar <51852012+NI-Tushar@users.noreply.github.com>
2024-05-13 04:05:04 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar option1='Dropdown' option2="React" />
+      {/* <Navbar /> */}
+    </>
+  );
+}
+
+export default App;
